b300c7d65582ba0cabec8f18b96ccb3ad353a123
Sai <Sai@hi>
2020-03-03 15:18:53 +0530

    Project commit

---
+++ DemoMaven/src/main/java/com/javatest/DemoMaven/DemoMavenTest.java
@@ -0,0 +1,13 @@
+package com.javatest.DemoMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class DemoMavenTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!....sai" );
+    }
+}
